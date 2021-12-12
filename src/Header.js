f6f092f369c0e5aab9dc21373470a2bde93cea4e
Gursimran Singh <gurisingh262001@gmail.com>
2021-12-12 15:55:43 +0530

    header file initialized and material ui added

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import { Menu } from '@material-ui/core';
+import React from 'react';
+
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>IM HEADER</h1>
+            <Menu />
+        </div>
+    )
+}
+
+export default Header
