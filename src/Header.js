16fb237a01259c54fe9945d549a7f3e24ee1b918
Gursimran Singh <gurisingh262001@gmail.com>
2021-12-13 00:29:32 +0530

    Header created

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,33 @@
-import { Menu } from '@material-ui/core';
+
 import React from 'react';
+import Menu from '@material-ui/icons/Menu';
+import { Apps, Notifications, Search, VideoCall } from '@material-ui/icons';
+import logo from './media/logo.png';
+import { Avatar } from '@material-ui/core';
+import avatar from './media/avatar.png';
+import './Header.css';
 
 
 function Header() {
     return (
-        <div className='header'>
-            <h1>IM HEADER</h1>
-            <Menu />
+        <div className='flexh header'>
+            <div className='flexh header_firstchild'>
+                <Menu className='menubar'/>
+                <img className='header_logo' src={logo}  alt="logo"/>
+            </div>
+            
+            <div className='flexh header_search'>
+                <input className='search_i' type="text"/>
+                <Search className='search_btn'/>
+            </div>
+
+             <div className='flexh header_thirdchild'>
+                <Apps/>
+                <VideoCall/>
+                <Notifications />
+                <Avatar alt="incredibleHulk" src={avatar}/>
+             </div>
+            
         </div>
     )
 }
