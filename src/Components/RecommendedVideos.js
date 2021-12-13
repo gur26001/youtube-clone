9ba66bcd4ca9dac5d55f298b7b72d0e10f24fa55
Gursimran Singh <gurisingh262001@gmail.com>
2021-12-13 11:35:37 +0530

    sidebar and recommended video section initialized

---
+++ src/Components/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import './RecommendedVideos.css';
+import React from 'react'
+
+function RecommendedVideos() {
+    return (
+        <div className='recommended'>
+           <h1>All videos goes here.</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideos
